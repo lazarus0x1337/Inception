73dc9a01104fdbf02d4745440d36aa530f8435d1
lazarus1337 <abdellah.fanidi@edu.uiz.ac.ma>
2025-01-02 10:21:09 -0500

    modify default.conf for correct paths in wordpress

--- srcs/requirements/wordpress/tools/WordpressScript.sh
+++ srcs/requirements/wordpress/tools/WordpressScript.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
-# exit when script returns a non-zero exit status
-set -e
-
-
-if [ ! -e .firstbuild ]; then
-    echo "installing wp cli..."
-    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
-    chmod +x wp-cli.phar 
-    mv wp-cli.phar /usr/local/bin/wp
-    touch .firstbuild
-fi
 
 # #Removing the 127.0.0.1 to listen on all available interfaces at port 9000
 sed -i 's/listen = 127.0.0.1:9000/listen = 9000/g' /etc/php83/php-fpm.d/www.conf
 cd  /var/www/html
-if [ ! -f wp-config.php ]; then
-        echo "Installing WordPress..."
+if [ ! -e .firstbuild ]; then
+        echo "WordPress installation process..."
         wp core download --allow-root || true
         
         # Create wp-config.php
@@ -32,6 +21,7 @@ if [ ! -f wp-config.php ]; then
         wp config set FS_METHOD direct
 
         #Installing WordPress site with configure admin user : 
+        
         wp core install --allow-root \
         --url="$DOMAIN_NAME" \
         --title="$WORDPRESS_TITLE" \
@@ -40,10 +30,8 @@ if [ ! -f wp-config.php ]; then
         --admin_email="$WORDPRESS_ADMIN_EMAIL" \
         --skip-email # dont send email 
 
-        # Create a regular user if it doesn't already exist
-        if ! wp user get "$WORDPRESS_USER" --allow-root > /dev/null 2>&1; then
-            wp user create "$WORDPRESS_USER" "$WORDPRESS_EMAIL" --role=author --user_pass="$WORDPRESS_PASSWORD" --allow-root
-        fi
+        wp user create "$WORDPRESS_USER" "$WORDPRESS_EMAIL" --role=author --user_pass="$WORDPRESS_PASSWORD" --allow-root
+        touch .firstbuild
 else
         echo "WordPress is already installed."
 fi
@@ -52,4 +40,4 @@ chown -R nobody:nobody /var/www/html
 
 exec php-fpm83 -F
 
-echo "Starting PHP-FPM..."
+
